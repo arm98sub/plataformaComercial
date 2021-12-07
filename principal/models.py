372e6c0df819bc45e436baf1252bd49855645a0d
DanielVls33 <89718583+DanielVls33@users.noreply.github.com>
2021-12-06 18:41:34 -0600

    Push

--- principal/models.py
+++ principal/models.py
@@ -1,23 +1,43 @@
 from django.db import models
-from django.conf import settings
-
-
 
 class Producto(models.Model):
-	vendedor = models.ForeignKey('usuarios.Usuario_Vendedor', verbose_name='Vendedor', on_delete=models.CASCADE)
-	nombre = models.CharField(max_length=30, blank=False, null=False)
-	imagen = models.ImageField("Imagen", upload_to='principal', blank=True, null=True)
-	precio = models.DecimalField(max_digits=10, decimal_places=2)
-	stock = models.PositiveIntegerField(default=0)
-	categoria = models.CharField(max_length=50, blank=False, null=False)
-	descripcion = models.CharField("Descripción", max_length=250, null=True, blank=True)
+    vendedor = models.ForeignKey(
+        'usuarios.Usuario_Vendedor',
+        verbose_name='Vendedor',
+        on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=30, blank=False, null=False)
+    imagen = models.ImageField(
+        "Imagen",
+        upload_to='principal',
+        blank=True,
+        null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    categoria = models.CharField(max_length=50, blank=False, null=False)
+    descripcion = models.CharField(
+        "Descripción",
+        max_length=250,
+        null=True,
+        blank=True)
 
-	def __str__(self):
-		return self.nombre
+    def __str__(self):
+        return self.nombre
+
 
 class Servicio(models.Model):
-	vendedor = models.ForeignKey('usuarios.Usuario', verbose_name='Vendedor', on_delete=models.CASCADE)
-	nombre = models.CharField(max_length=50, blank=False, null=False)
-	imagen = models.ImageField("Imagen", upload_to='principal', blank=True, null=True)
-	descripcion = models.TextField("Descripción", max_length=250, null=True, blank=True)
-	categoria = models.CharField(max_length=50, blank=False, null=False)
+    vendedor = models.ForeignKey(
+        'usuarios.Usuario',
+        verbose_name='Vendedor',
+        on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50, blank=False, null=False)
+    imagen = models.ImageField(
+        "Imagen",
+        upload_to='principal',
+        blank=True,
+        null=True)
+    descripcion = models.TextField(
+        "Descripción",
+        max_length=250,
+        null=True,
+        blank=True)
+    categoria = models.CharField(max_length=50, blank=False, null=False)
--- principal/views.py
+++ principal/views.py
@@ -1,118 +1,117 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.http import request
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-
-from usuarios.models import Usuario
-
 from .forms import ProductoForm, ProductoFormVendedor, ServicioForm
 from .models import Producto, Servicio
 
-
 # Home del sitio
-
 def index(request):
-	productos = Producto.objects.all()[:3]
-	servicios = Servicio.objects.all()[:3]
-    
-	return render(request, 'index.html', {'productos': productos, 'servicios': servicios})
+    productos = Producto.objects.all()[:3]
+    servicios = Servicio.objects.all()[:3]
+
+    return render(
+        request, 'index.html', {
+            'productos': productos, 'servicios': servicios})
+
 
 def admin(request):
-	return render(request, 'admin.html', {})
+    return render(request, 'admin.html', {})
 
 
 # Listado de productos/servicios
-
 def lista(request):
-	productos = Producto.objects.all()
-	servicios = Servicio.objects.all()
-	
-	return render(request, 'principal/lista.html', {'productos': productos, 'servicios': servicios})
+    productos = Producto.objects.all()
+    servicios = Servicio.objects.all()
+
+    return render(request, 'principal/lista.html',
+                  {'productos': productos, 'servicios': servicios})
 
 # Listado de productos/servicios para administradores
-
 def lista_admin(request):
-	productos = Producto.objects.all()
-	servicios = Servicio.objects.all()
-	
-	return render(request, 'principal/lista_admin.html', {'productos': productos, 'servicios': servicios})
+    productos = Producto.objects.all()
+    servicios = Servicio.objects.all()
+
+    return render(request, 'principal/lista_admin.html',
+                  {'productos': productos, 'servicios': servicios})
 
 # CRUD Productos
-
 class NuevoProducto(PermissionRequiredMixin, CreateView):
-	permission_required = 'usuarios.permiso'
-	model = Producto
-	form_class = ProductoForm
-	template_name = 'principal/nuevo_producto.html'
-	success_url = reverse_lazy('principal:lista_admin')
+    permission_required = 'usuarios.permiso'
+    model = Producto
+    form_class = ProductoForm
+    template_name = 'principal/nuevo_producto.html'
+    success_url = reverse_lazy('principal:lista_admin')
 
-	extra_context = {
-		'etiqueta': "Nuevo",
-		'boton': "Agregar",
-	}
+    extra_context = {
+        'etiqueta': "Nuevo",
+        'boton': "Agregar",
+    }
+
 
 class NuevoProductoVendedor(CreateView):
-	# permission_required = 'usuarios.'
-	template_name = 'principal\producto_form_vendedor.html'
-	model = Producto
-	form_class = ProductoFormVendedor
-	success_url = reverse_lazy('principal/lista')
-	extra_context = {
-		'etiqueta': "Nuevo",
-		'boton': "Agregar",
-	}
-	
+    template_name = 'principal\\producto_form_vendedor.html'
+    model = Producto
+    form_class = ProductoFormVendedor
+    success_url = reverse_lazy('principal/lista')
+    extra_context = {
+        'etiqueta': "Nuevo",
+        'boton': "Agregar",
+    }
 
 
 class EditarProducto(PermissionRequiredMixin, UpdateView):
-	permission_required = 'usuarios.permiso_administradores'
-	model = Producto
-	form_class = ProductoForm
-	success_url = reverse_lazy('principal:lista_admin')
+    permission_required = 'usuarios.permiso_administradores'
+    model = Producto
+    form_class = ProductoForm
+    success_url = reverse_lazy('principal:lista_admin')
 
-	extra_context = {
-		'etiqueta': "Actualizar",
-		'boton': "Guardar",
-	}
+    extra_context = {
+        'etiqueta': "Actualizar",
+        'boton': "Guardar",
+    }
+
 
 class EliminarProducto(PermissionRequiredMixin, DeleteView):
-	permission_required = 'usuarios.permiso_administradores'
-	model = Producto
-	success_url = reverse_lazy('principal:lista_admin')
+    permission_required = 'usuarios.permiso_administradores'
+    model = Producto
+    success_url = reverse_lazy('principal:lista_admin')
+
 
 class VerProducto(DetailView):
-	model = Producto
-	template_name = "principal/detalleProducto.html"
-	context_object_name = "producto"
+    model = Producto
+    template_name = "principal/detalleProducto.html"
+    context_object_name = "producto"
 
 # CRUD Servicios
 
-class NuevoServicio(PermissionRequiredMixin, CreateView):
-	permission_required = 'usuarios.permiso_administradores'
-	model = Servicio
-	form_class = ServicioForm
-	success_url = reverse_lazy('principal:lista_admin')
 
-	extra_context = {
-		'etiqueta': "Nuevo",
-		'boton': "Agregar",
-	}
+class NuevoServicio(PermissionRequiredMixin, CreateView):
+    permission_required = 'usuarios.permiso_administradores'
+    model = Servicio
+    form_class = ServicioForm
+    success_url = reverse_lazy('principal:lista_admin')
+
+    extra_context = {
+        'etiqueta': "Nuevo",
+        'boton': "Agregar",
+    }
+
 
 class EditarServicio(PermissionRequiredMixin, UpdateView):
-	permission_required = 'usuarios.permiso_administradores'
-	model = Servicio
-	form_class = ServicioForm
-	success_url = reverse_lazy('principal:lista_admin')
+    permission_required = 'usuarios.permiso_administradores'
+    model = Servicio
+    form_class = ServicioForm
+    success_url = reverse_lazy('principal:lista_admin')
 
-	extra_context = {
-		'etiqueta': "Actualizar",
-		'boton': "Guardar",
-	}
+    extra_context = {
+        'etiqueta': "Actualizar",
+        'boton': "Guardar",
+    }
+
 
 class EliminarServicio(PermissionRequiredMixin, DeleteView):
-	permission_required = 'usuarios.permiso_administradores'
-	model = Servicio
-	success_url = reverse_lazy('principal:lista_admin')
-
+    permission_required = 'usuarios.permiso_administradores'
+    model = Servicio
+    success_url = reverse_lazy('principal:lista_admin')
