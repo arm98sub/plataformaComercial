ebcf977c500f67fd42671b0dc7a7b217bdb3fd71
Salatiel Reyes Gaytan <33143501@uaz.edu.mx>
2022-04-18 14:37:25 -0500

    Cambios al carrusel

--- static/js/funciones.js
+++ static/js/funciones.js
@@ -64,3 +64,42 @@ $("#show-sidebar").click(function() {
    
    
 });
+
+const myCarousel = document.getElementById("carouselIndex");
+const carouselIndicators = myCarousel.querySelectorAll(
+  ".carousel-indicators button span"
+);
+let intervalID;
+
+const carousel = new bootstrap.Carousel(myCarousel);
+
+window.addEventListener("load", function () {
+  fillCarouselIndicator(1);
+});
+
+myCarousel.addEventListener("slide.bs.carousel", function (e) {
+  let index = e.to;
+  fillCarouselIndicator(++index);
+});
+
+function fillCarouselIndicator(index) {
+  let i = 0;
+  for (const carouselIndicator of carouselIndicators) {
+    carouselIndicator.style.width = 0;
+  }
+  clearInterval(intervalID);
+  carousel.pause();
+
+  intervalID = setInterval(function () {
+    i++;
+
+    myCarousel.querySelector(".carousel-indicators .active span").style.width =
+      i + "%";
+
+    if (i >= 100) {
+      // i = 0; -> just in case
+      carousel.next();
+    }
+  }, 50);
+}
+
