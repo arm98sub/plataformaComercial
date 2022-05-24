2275e85209e16dca65680f3218786bcc81bad382
Salatiel Reyes Gaytan <33143501@uaz.edu.mx>
2022-05-23 21:01:41 -0500

    No se

--- comerciov2/settings.py
+++ comerciov2/settings.py
@@ -34,7 +34,9 @@ INSTALLED_APPS = [
     'usuarios',
     'crispy_forms',
 ]
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
