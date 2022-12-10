8129f164a5c3df47d5674dbf645de65ac19f230c
Salatiel Reyes Gaytan <33143501@uaz.edu.mx>
2022-12-10 15:13:08 -0600

    Cambio de slug por id en las url.

--- ordenes/urls.py
+++ ordenes/urls.py
@@ -1,14 +1,13 @@
-from django.urls import path
-from . import views
 from django.urls import path, include
+from . import views
 
 app_name = 'ordenes'
 
 urlpatterns = [
     path('', include('principal.urls')),
-    path('agregar/<slug>/', views.agregar_carrito, name="agregar"),
-    path('eliminar-de-carrito/<slug>/', views.eliminar_de_carrito, name = 'eliminar-de-carrito'),
-    path('eliminar-articulo-carrito/<slug>/', views.eliminar_un_producto_del_carrito, 
+    path('agregar/<pk>/', views.agregar_carrito, name="agregar"),
+    path('eliminar-de-carrito/<pk>/', views.eliminar_de_carrito, name='eliminar-de-carrito'),
+    path('eliminar-articulo-carrito/<pk>/', views.eliminar_un_producto_del_carrito,
          name="eliminar-articulo-carrito"),
     path('carrito/', views.lista_carrito.as_view(), name="lista_carrito"),
     path('comprar/', views.comprar, name="comprar"),
